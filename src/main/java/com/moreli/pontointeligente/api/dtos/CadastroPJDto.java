1deed666f971d09e56c0567abfcd87df7b27c334
Moreli04 <leosmoreli@gmail.com>
2020-02-16 15:04:58 -0300

    Criando controllers

---
+++ src/main/java/com/moreli/pontointeligente/api/dtos/CadastroPJDto.java
@@ -0,0 +1,47 @@
+package com.moreli.pontointeligente.api.dtos;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CNPJ;
+import org.hibernate.validator.constraints.br.CPF;
+
+import lombok.Data;
+
+@Data
+public class CadastroPJDto {
+	
+	private Long id;
+	
+	@NotEmpty(message = "Nome não pode ser vazio.")
+	@Length(min = 3, max = 200, message = "Nome deve conter entre 3 e 200 caracteres.")
+	private String nome;
+	
+	@NotEmpty(message = "Email não pode ser vazio.")
+	@Length(min = 5, max = 200, message = "Email deve conter entre 5 e 200 caracteres.")
+	@Email(message="Email inválido.")
+	private String email;
+	
+	@NotEmpty(message = "Senha não pode ser vazia.")
+	private String senha;
+	
+	@NotEmpty(message = "CPF não pode ser vazio.")
+	@CPF(message="CPF inválido")
+	private String cpf;
+	
+	@NotEmpty(message = "Razão social não pode ser vazio.")
+	@Length(min = 5, max = 200, message = "Razão social deve conter entre 5 e 200 caracteres.")
+	private String razaoSocial;
+	
+	@NotEmpty(message = "CNPJ não pode ser vazio.")
+	@CNPJ(message="CNPJ inválido.")
+	private String cnpj;
+
+	@Override
+	public String toString() {
+		return "CadastroPJDto [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + ", cpf=" + cpf
+				+ ", razaoSocial=" + razaoSocial + ", cnpj=" + cnpj + "]";
+	}
+
+}
