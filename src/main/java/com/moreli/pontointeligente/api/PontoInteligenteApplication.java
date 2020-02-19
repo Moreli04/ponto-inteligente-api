ca825aeb3942ab70af3d3bf13b28ebbc38b427fc
Moreli04 <leosmoreli@gmail.com>
2020-02-19 20:50:53 -0300

    habilitando ehcache

--- src/main/java/com/moreli/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/moreli/pontointeligente/api/PontoInteligenteApplication.java
@@ -2,9 +2,11 @@ package com.moreli.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 //@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @SpringBootApplication
+@EnableCaching
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
